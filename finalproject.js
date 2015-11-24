218d46af357b880f503241babbd1549332a85a53
mlpinlac <mlpinlac@hawaii.edu>
2015-11-23 16:20:01 -1000

    Milestone #2 Check in - Added Iron Router
    For Milestone #2 check in:
    1. Added iron router
    2. Figured out how to add multi pages using router

--- finalproject.js
+++ finalproject.js
@@ -1,3 +1,12 @@
+Router.route('/', {
+  template: 'homecontent'
+});
+
+Router.route('/register');
+Router.route('/login');
+Router.route('/schedule');
+Router.route('/calculator');
+
 if (Meteor.isClient) {
 
 }
