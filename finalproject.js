57925d5410e2f3334bec8499cb546a16ecc02d28
mlpinlac <mlpinlac@hawaii.edu>
2015-11-24 01:23:41 -1000

    Milestone Checkin #2: User accounts continued
    For Milestone Checkin #2:
    1. Figured out how to save data specifically for logged in users

--- finalproject.js
+++ finalproject.js
@@ -7,6 +7,17 @@ Router.route('/login');
 Router.route('/schedule');
 Router.route('/calculator');
 
+Router.route('/list/:_id', {
+  name: 'listPage',
+  template: 'listPage',
+  data: function(){
+    var currentList = this.params._id;
+    return Lists.findOne({ _id: currentList });
+  }
+});
+
+Todos = new Mongo.Collection('todos');
+Lists = new Meteor.Collection('lists');
 
 if (Meteor.isClient) {
   Template.register.events({
@@ -18,8 +29,109 @@ if (Meteor.isClient) {
         email: email,
         password: password
       });
-
       Router.go('/');
     }
   });
+
+  Template.nav.events({
+    'click .logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+      Router.go('login');
+    }
+  });
+
+  Template.nav2.events({
+    'click .logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+      Router.go('login');
+    }
+  });
+
+  Template.nav3.events({
+    'click .logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+      Router.go('login');
+    }
+  });
+  Template.login.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Meteor.loginWithPassword(email, password, function(error){
+        if(error){
+          // function() is to handle errors with login
+          console.log(error.reason);
+        } else{
+          Router.go('/');
+        }
+      });
+    }
+  });
+
+  Template.todos.helpers({
+    'todo': function(){
+      var currentList = this._id;
+      var currentUser = Meteor.userId();
+      return Todos.find({ listId: currentList, createdBy: currentUser }, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.addTodo.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var todoName = $('[name="todoName"]').val();
+      var currentUser = Meteor.userId();
+      var currentList = this._id;
+      Todos.insert({
+        name: todoName,
+        completed: false,
+        createdAt: new Date(),
+        createdBy: currentUser,
+        listId: currentList
+      });
+      $('[name="todoName"]').val('');
+    }
+  });
+
+  Template.todoItem.events({
+    'click .delete-todo': function(event){
+      event.preventDefault();
+      var documentId = this._id;
+      var confirm = window.confirm("Remove this task?");
+      if(confirm){
+        Todos.remove({ _id: documentId });
+      }
+    }
+  });
+
+  Template.addList.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var listName = $('[name=listName]').val();
+      var currentUser = Meteor.userId();
+      Lists.insert({
+        name: listName,
+        createdBy: currentUser
+      },
+          function(error, results){
+            Router.go('listPage', { _id: results });
+      });
+      $('[name=listName]').val('');
+    }
+  });
+
+  Template.lists.helpers({
+    'list': function(){
+      var currentUser = Meteor.userId();
+      return Lists.find({ createdBy: currentUser }, {sort: {name: 1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
 }
